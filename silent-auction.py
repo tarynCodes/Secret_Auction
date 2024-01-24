9fe1e057987073c9148f2e4eb21086d8d6b51d3b
taryncodes@github.com <taryn.bilsborough@gmail.com>
2024-01-24 16:59:02 +0000

    creates while loop, and set bidding to true, input statements to get users name and bid amount then asks if anymore bids need to be inputted. if yes re-runs program, if no finds the highest bid and prints

--- silent-auction.py
+++ silent-auction.py
@@ -10,3 +10,27 @@ bids = {
 def silent_auction(person, amount):
     bids[person] = amount
 
+
+bidding = True
+
+
+while bidding:
+    print(logo)
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? £"))
+    more_bids = input("Is there any more bids? Yes or No? ").lower()
+
+    silent_auction(name, bid)
+
+    if more_bids == "yes":
+        os.system('clear' if os.name == 'posix' else 'cls')
+    else:
+        bidding = False
+        winner = ""
+        highest_bid = 0
+        for bidder in bids:
+            bid = bids[bidder]
+            if bid > highest_bid:
+                highest_bid = bid
+                winner = bidder
+            print(f"The winner is {winner} with a bid amount of £{highest_bid}")
